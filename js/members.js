613ac91f9889c568fdb8ef17ae5266a715ee46f2
Sudiptaguru <64124103+Sudiptaguru@users.noreply.github.com>
2022-02-20 20:10:57 +0530

    Update members.js

--- js/members.js
+++ js/members.js
@@ -2,74 +2,6 @@ $(document).ready( function() {
   setTimeout('$("#alert_msg").hide()',3000);
 });
 
-function check(id){
-
-
-
-try{
-
-    //alert(id);
-
-    $.ajax({
-
-            type : "POST",
-
-            url : "<?php echo ADMIN_URL; ?>ajax/members_ajax.php",
-
-            dataType : "json", 
-
-            data : "id="+id,
-
-            success : function(data) {            
-
-              //alert(data.flag);
-
-              try{
-
-                
-
-                 $('#draggable').modal('show');                 
-
-                 $("#name").val(data.name);
-
-                 $("#email").val(data.email);
-
-                 $("#phone").val(data.phone);
-
-                 $("#id").val(data.id);               
-
-                
-
-              }
-
-              catch(err){
-
-                alert(err.message);
-
-              }
-
-            
-
-            }
-
-          });
-
-        }catch(err){
-
-          alert(err.message);
-
-        }
-
-setInterval(function(){
-
-   $('#error_msg').html('');
-
-  }, 5000);
-
- 
-
-}
-
 function validateForm() {
 
   let name = document.forms["myForm"]["name"].value;
@@ -103,33 +35,3 @@ function validateForm() {
   }
 
 }
-
-$('input[name=onoffswitch]').click(function(){
-
-var id=$(this).attr('id');
-
-var crm_access = $(this).val();
-
-if(crm_access == 1) {
-
-    crm_access = 0; 
-
-} else {
-
-    crm_access = 1; 
-
-}
-
-//alert(id);
-
-$.ajax({
-
-        type:'POST',
-
-        url:'active_deactive.php',
-
-        data:'id= ' + id + '&crm_access='+crm_access
-
-    });
-
-}); 
